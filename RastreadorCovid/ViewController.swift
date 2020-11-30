53aad39f439e65ae25a2ec4ace92cafd5505b144
Miguel Angel <magragera@alu.ucam.edu>
2020-11-30 16:15:28 +0100

    Initial Commit

---
+++ RastreadorCovid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RastreadorCovid
+//
+//  Created by Miguel Ángel Gragera García on 30/11/2020.
+//  Copyright © 2020 Miguel Ángel Gragera García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
